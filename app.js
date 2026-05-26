0dd797a47cf4e0f83bc32bef19b55248413aebf0
himankclg <himankyadav_cse_25a03005@dtu.ac.in>
2026-05-26 22:49:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views" , path.join(__dirname,"/views")); // defining path of views folder dirname shows index.js file is where
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+})
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+})
+
+
+app.get("/form", (req, res) => {
+    res.render("form.ejs");
+})
+
+
+app.listen(8080, () => {
+    console.log("listening on port 8080");
+});
